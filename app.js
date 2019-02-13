6bd639cf137a5b18f5f7b2b9e20bf45748bedf7e
huzhengxin <1083631293@qq.com>
2019-02-13 23:05:16 +0800

    add some files

---
+++ app.js
@@ -0,0 +1,47 @@
+//app.js
+App({
+  onLaunch: function () {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+    var that = this;
+    wx.login({
+      success: res => {
+        wx.request({
+          url: that.globalData.wx_url_1 + res.code + that.globalData.wx_url_2,
+          header: {
+            'content-type': 'application/json'
+          },
+          success: res => {
+            that.globalData.openid = res.data.openid;
+            //console.log(res.data.openid)
+            //console.log(that.globalData.openid)
+            //获取用户id
+            wx.request({
+              url: 'http://192.168.94.1:3000/addId',
+              data: { openid: res.data.openid },
+              success: (res) => {
+                //console.log(res)
+                if (res.data.data[0]) {
+                  that.globalData.uid = res.data.data[0].uid
+                } else {
+                  that.globalData.uid = 0;
+                }
+              }
+            });
+          }
+        })
+      }
+    });
+  },
+  globalData: {
+    userInfo: null,
+    loginName: null,
+    urlPath: null,
+    openid: 0,
+    uid: "",
+    wx_url_1: 'https://api.weixin.qq.com/sns/jscode2session?appid=wxc0369836b29cd663&secret=52885f51ca166b0593833f26fb738315&js_code=',
+    wx_url_2: '&grant_type=authorization_code'
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,200 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+
+Page({
+  //跳转搜索
+  select: function () {
+    wx: wx.navigateTo({
+      url: '/pages/select/select',
+    })
+  },
+  //跳转个人中心
+  profile: function () {
+    wx: wx.navigateTo({
+      url: '/pages/profile/profile',
+    })
+  },
+  //跳转购物车
+  shopCard: function () {
+    wx.getSetting({
+      success: (res) => {
+        if (res.authSetting['scope.userInfo']) {
+          wx: wx.navigateTo({
+            url: '/pages/shopCart/shopCart',
+          })
+        } else {
+          wx: wx.navigateTo({
+            url: '/pages/login/login',
+          })
+        }
+      }
+    })
+  },
+  //刷新首页
+  indexBtn() {
+    wx: wx.redirectTo({
+      url: '/pages/index/index',
+    })
+  },
+  //跳转详情页
+  ring: function (e) {
+    var id = e.target.dataset.id;
+    wx: wx.navigateTo({
+      url: '/pages/detail/detail?id=' + id,
+    })
+  },
+  loadMore: function () {
+    //1.获取二个数值 pno pageSize
+    var pno = this.data.pageIndex + 1;
+    var ps = this.data.pageSize;
+    var hasMore = pno <= this.data.pageCount
+    if (!hasMore) { return }
+    //console.log(123)
+    //2.发送ajax请求
+    wx.request({
+      url: 'http://192.168.42.167:3000/indexshops',
+      data: { pno: pno, pageSize: ps },
+      success: (result) => {
+        //2.1保存返回数据data
+        //console.log(result.data.data);
+        var rows = this.data.nav3list.concat(
+          result.data.data
+        );
+
+        this.setData({
+          nav3list: rows,
+          pageIndex: pno,
+          pageCount: result.data.pageCount
+        });
+      },
+    })
+  },
+  handleJump: function (e) {
+    var id = e.target.dataset.id;
+    var name = e.target.dataset.name;
+    wx: wx.navigateTo({
+      url: '/pages/shoplist/shoplist?id=' + id + "&name=" + name,
+    })
+
+
+  },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [],
+    navlist: [],
+    nav1list: [],
+    src: "http://192.168.42.167:3000/img/png/icon_ser.jpg",
+    nav3list: [],
+    pageIndex: 0,
+    pageSize: 8,
+    pageCount: 1
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+    wx.request({
+      url: 'http://192.168.42.167:3000/getBanner',
+      data: {},
+      success: (res) => {
+        this.setData({
+          list: res.data.data,
+        })
+      }
+    });
+    wx.request({
+      url: 'http://192.168.42.167:3000/getnav',
+      data: {},
+      success: (res) => {
+        //console.log(res)
+        this.setData({
+          navlist: res.data.data
+        })
+      }
+    });
+    wx.request({
+      url: 'http://192.168.42.167:3000/getnav1',
+      data: {},
+      success: (res) => {
+        //console.log(res)
+        this.setData({
+          nav1list: res.data.data
+        })
+      }
+    });
+    this.loadMore();
+    // 查看是否授权
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: function (res) {
+              //获取用户信息
+              app.globalData.userInfo = res.userInfo
+              //console.log(app.globalData.userInfo)
+            }
+          });
+        } else {
+          wx: wx.navigateTo({
+            url: '/pages/login/login',
+          })
+        }
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    this.loadMore()
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
